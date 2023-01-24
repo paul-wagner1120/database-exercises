dedb8277c5d5223b0c8f26ffa62a260cb952080d
Paul Wagner <p.wagner1120@icloud.com>
2023-01-24 14:21:47 -0600

    adding changes to select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 
-    SELECT * from albums where artist_name = 'Pink Floyd';
-    SELECT release_date from albums where id = 28;
-    SELECT * from albums where release_date BETWEEN 1990 and 1999;
-    SELECT * from albums where sales < 20;
-    SELECT * FROM albums WHERE genre = 'Rock';
+    SELECT album_name AS 'Albums by Pink Floyd' from albums where artist_name = 'Pink Floyd';
+    SELECT release_date AS 'Release Date for Sgt Peppers Lonely Hearts Club Band' from albums where id = 28;
+    SELECT artist_name AS 'Artist', album_name AS 'Albums released between 1990 and 1999'from albums where release_date BETWEEN 1990 and 1999;
+    SELECT artist_name AS 'Artist', album_name AS 'Album' from albums where sales < 20;
+    SELECT artist_name AS 'Artist', album_name AS 'Album' FROM albums WHERE genre = 'Rock';
