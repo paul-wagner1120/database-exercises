894c0a78f676c4c33560e2f82e807652d1fd34ef
Paul Wagner <p.wagner1120@icloud.com>
2023-01-25 10:52:35 -0600

    changing migration file

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist_name VARCHAR(50),
-                        album_name  VARCHAR(100),
+                        artist_name VARCHAR(255),
+                        album_name  VARCHAR(255),
                         release_date int,
                         sales float,
-                        genre VARCHAR(50),
+                        genre VARCHAR(255),
                         PRIMARY KEY (id)
 );
