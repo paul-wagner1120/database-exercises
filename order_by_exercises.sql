8ce70afacdf9fc73edcda64bdcc683a95c9f28b2
Paul Wagner <p.wagner1120@icloud.com>
2023-01-25 16:23:05 -0600

    adding order by exercises and finished

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,10 +1,14 @@
 use employees;
 
 SELECT *  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name; # order by #2
+    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name; # order by #3
+    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name; # order by #4
+
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
-
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no DESC; #order by #6
 SELECT *  FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
