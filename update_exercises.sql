437e9d74aca9ae712630086e7d9af490a5cbe6af
Paul Wagner <p.wagner1120@icloud.com>
2023-01-25 09:39:50 -0600

    fixing update exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,7 +6,7 @@ USE codeup_test_db;
 
         update albums set sales = sales*10;
             select * from albums;
-        update albums set release_date = 1800 where release_date < 1980;
+        update albums set release_date = release_date - 100 where release_date < 1980;
             select * from albums where release_date < 1980;
         update albums set artist_name = 'Peter Jackson' where artist_name = 'Michael Jackson';
             select * from albums where artist_name = 'Peter Jackson';
