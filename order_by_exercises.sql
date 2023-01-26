fdcc283345d317477969c5ed2d2c664c3803a488
Paul Wagner <p.wagner1120@icloud.com>
2023-01-26 09:17:17 -0600

    adding a couple changes to order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,15 +4,11 @@ SELECT *  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
     SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name; # order by #2
     SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name; # order by #3
     SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name; # order by #4
-
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
-
-SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no DESC; #order by #6
-SELECT *  FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+    SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC , first_name DESC; # order by #6
 
 
-SELECT *  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
-SELECT * FROM employees WHERE last_name LIKE 'E%E';
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+
+SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no; #order by #5
+SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no DESC; #order by #6
+
+
