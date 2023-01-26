fb21f0f3ceb1d1a2c02c60d0ce288cb0b4f3743f
Paul Wagner <p.wagner1120@icloud.com>
2023-01-26 10:14:20 -0600

    deleting unused code from functions exercise

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -1,17 +1,5 @@
 use employees;
 
-# SELECT *  FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name; # order by #2
-# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name; # order by #3
-# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name; # order by #4
-# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC , first_name DESC; # order by #6
-#
-#
-#
-# SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no; #order by #5
-# SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no DESC; #order by #6
-
-
 SELECT CONCAT(first_name, ' ', last_name) AS 'Name' FROM employees WHERE last_name LIKE 'e%e' AND first_name LIKE 'E%E';
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
