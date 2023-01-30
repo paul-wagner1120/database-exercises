8b40a5f8025925cb7ec13c421c4d35773452b42a
Paul Wagner <p.wagner1120@icloud.com>
2023-01-30 09:28:19 -0600

    making changes to aliases exercises

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
-SELECT CONCAT(first_name, ' ', last_name) AS 'full_name' FROM employees LIMIT 10;
+SELECT CONCAT(last_name, ', ', first_name) AS 'full_name' FROM employees LIMIT 10;
 
-SELECT CONCAT(first_name, ' ', last_name) AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 10;
+SELECT CONCAT(last_name, ', ', first_name) AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 10;
 
-SELECT CONCAT(emp_no, '-', first_name, last_name) AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 10;
+SELECT CONCAT(emp_no, '-', last_name, ', ', first_name) AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 10;
